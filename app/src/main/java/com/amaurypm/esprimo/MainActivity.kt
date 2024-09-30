be26675311f5b68853aed46d0e27383cf28c690f
Amaury Perea Matsumura <amaury.matsumura@gmail.com>
2024-09-30 10:40:58 -0600

    Commit inicial

---
+++ app/src/main/java/com/amaurypm/esprimo/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.amaurypm.esprimo
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.amaurypm.esprimo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnVerificar.setOnClickListener {
+            //click al botón
+
+            if (binding.etNumero.text.toString().isNotEmpty()) {
+
+                val numero = binding.etNumero.text.toString().toInt()
+
+                val character = "!"
+
+                if (esPrimo(numero)) {
+                    binding.tvResultado.text = getString(R.string.si_primo, numero, character)
+                } else {
+                    binding.tvResultado.text = getString(R.string.no_primo, numero)
+                }
+            }else{
+                Toast.makeText(
+                    this,
+                    getString(R.string.ingresa_valor),
+                    Toast.LENGTH_SHORT
+                ).show()
+                binding.etNumero.error = getString(R.string.valor_requerido)
+                binding.etNumero.requestFocus()
+            }
+        }
+
+    }
+
+    private fun esPrimo(num: Int): Boolean {
+        if (num <= 1) return false
+
+        for (i in 2 until num) {
+            if (num % i == 0) return false
+        }
+
+        return true
+    }
+}
---
+++ app/src/main/java/com/amaurypm/esprimo/StartActivity.kt
@@ -0,0 +1,30 @@
+package com.amaurypm.esprimo
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.amaurypm.esprimo.databinding.ActivityStartBinding
+
+class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStartBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.btnStart.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
